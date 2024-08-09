e8399cd3a978fa1c5cf20643ecd0fc6e665f6d45
jlcastro1877 <jlcastro1877@gmail.com>
2024-08-09 08:36:44 -0600

    Error handle added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 8000;
 
 connectDB();
@@ -14,5 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
