4d1266601a33009e9b07fd1ab3dfb546b3966486
jlcastro1877 <jlcastro1877@gmail.com>
2024-08-09 13:25:08 -0600

    Redux set up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import logo from "../assets/svg/logo.svg";
 // import logo from "../assets/Kasualelegant226.jpg";
 
 /**
